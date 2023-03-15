e49ef2b9e2a9d498e7a2e17a282abd9c29dc4fc1
Jo Wilks <jowilks@Jos-MacBook-Pro.local>
2023-03-15 08:48:22 +0000

    Add further comment App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import DashboardPage from './pages/dashboard/DashboardPage';
 
 /**
  * Decided to just use simple state of triggering page change, instead of react-router
- * However if required more pages would then include react-router
+ * However if required more pages would then include react-router.
+ * 
+ * Did not go with typescript (though for props it would've helped a lot to provide
+ * visibility). As I had to refresh myself on class components and scss for this task
+ * (I predominantly use hooks based and Styled Components in my current role).
  */
 class App extends React.Component {
   constructor() {
